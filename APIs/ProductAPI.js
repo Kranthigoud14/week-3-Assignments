31b4361ee3d3fbc20d3667dd4a7c660cfb5a0376
Kranthigoud14 <kranthigoudbalagoni14@gmail.com>
2026-03-13 09:45:54 +0530

    Update ProductAPI.js

--- APIs/ProductAPI.js
+++ APIs/ProductAPI.js
@@ -3,10 +3,8 @@ import { productModel } from "../models/productModules.js";
 export const productApp = exp.Router();
 // Create Product
 productApp.post("/products", async (req, res) => {
-
   const newProduct = new productModel(req.body);
   const savedProduct = await newProduct.save();
-
   res.status(201).json({
     message: "Product created",
     payload: savedProduct
@@ -14,53 +12,32 @@ productApp.post("/products", async (req, res) => {
 });
 // Read all products
 productApp.get("/products", async (req, res) => {
-
   const products = await productModel.find();
-
   res.status(200).json({
     message: "Products",
     payload: products
   });
 });
-
-
 // Read product by productID
 productApp.get("/products/:productID", async (req, res) => {
-
   const product = await productModel.findOne({ productID: req.params.productID});
-
   res.status(200).json({
     message: "Product",
     payload: product
   });
 });
-
-
 // Update product
 productApp.put("/products/:productID", async (req, res) => {
-
-  const updatedProduct = await productModel.findOneAndUpdate(
-    { productID: req.params.productID },
-    req.body,
-    { new: true }
+  const updatedProduct = await productModel.findOneAndUpdate({ productID: req.params.productID },req.body,{ new: true }
   );
-
   res.status(200).json({
     message: "Product updated",
     payload: updatedProduct
   });
 });
-
-
 // Delete product
 productApp.delete("/products/:productID", async (req, res) => {
-
-  const deletedProduct = await productModel.findOneAndDelete({
-    productID: req.params.productID
-  });
-
-  res.status(200).json({
-    message: "Product deleted",
-    payload: deletedProduct
+  const deletedProduct = await productModel.findOneAndDelete({  productID: req.params.productID  });
+  res.status(200).json({ message: "Product deleted",payload: deletedProduct
   });
 });
